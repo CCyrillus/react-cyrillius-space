86bc92dc180ce9632fe8cea596dc444d9f156fef
CCyrillus <kyrill.kiril@gmail.com>
2022-01-25 20:00:39 +0200

    Make action creator(give responsibility from UL to BLL) #2

---
+++ src/redux/store.js
@@ -0,0 +1,70 @@
+
+const ADD_POST = "ADD-POST";
+const UPDATE_NEW_POST = "UPDATE-NEW-POST";
+
+let store = {
+	_state: {
+		profilePage: {
+			postsData: [
+				{ message: "Hello React, it is my first post!)", likes: 11 },
+				{ message: "AS WE CAN!", likes: 21 },
+				{ message: "Light weight baby!!", likes: 33 },
+				{ message: "No Country for Old Men!)", likes: 19 },
+			],
+			newPostText: "",
+		},
+		dialogPage: {
+			dialogsData: [
+				{ id: 1, name: "Mikito" },
+				{ id: 2, name: "Simen" },
+				{ id: 3, name: "Hleb" },
+				{ id: 4, name: "Marianna" },
+				{ id: 5, name: "Sanchez" },
+				{ id: 6, name: "Alina" },
+			],
+
+			messagesData: [
+				{ id: 1, txt: "Hello!" },
+				{ id: 2, txt: "How are you?!" },
+				{ id: 1, txt: "Good! What about you?) What is new?" },
+				{ id: 2, txt: "YoYoYo" },
+				{ id: 2, txt: "AeAeAe" },
+				{ id: 2, txt: "MMM NICE" },
+			],
+		},
+	},
+	getState() {
+		return this._state;
+	},
+	_rerenderTree() {
+		console.log("QWERTY");
+	},
+	subscribe(observer) {
+		this._rerenderTree = observer;
+	},
+	dispatch(action) {
+		if (action.type === ADD_POST) {
+			let newPost = {
+				id: 5,
+				message: this.getState().profilePage.newPostText,
+				likes: Math.floor(Math.random() * 36),
+			};
+			this._state.profilePage.postsData.push(newPost);
+			this._state.profilePage.newPostText = "";
+			this._rerenderTree(this);
+		} else if (action.type === UPDATE_NEW_POST) {
+			this._state.profilePage.newPostText = action.text;
+			this._rerenderTree(this);
+		}
+	},
+};
+
+export const addPostCreator = () => ({ type: ADD_POST });
+
+export const updateNewPostCreator = (newText) => ({
+	type: UPDATE_NEW_POST,
+	text: newText,
+});
+
+export default store;
+window.store = store;
